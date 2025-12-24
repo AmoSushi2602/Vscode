63e153583cb09f28ff983580a72206c21bf1970e
João Mateus <joaomateus_2012@hotmail.com>
2025-12-24 11:07:07 -0300

    feat: Initialize project structure with Java settings and sample application

---
+++ GeradorLicenca/app/GeradorLicenca.java
@@ -0,0 +1,12 @@
+public class GeradorLicenca {
+
+    public static void main(String[] args){
+        String hwid = "A9D2-4C88-91D0";
+        String cliente = "Empresa XPTO";
+
+        String licenca = CryptoUtil.encrypt(
+            hwid + "|" + cliente
+        );
+        System.out.println(licenca);
+    }
+}
